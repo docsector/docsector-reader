0b34ab1b30b0925ccc38df289a27d7bb58eb2a9e
Rodrigo Vieira <rodrigo@slayer.tech>
2026-04-02 11:16:39 -0300

    release: v0.8.1

--- bin/docsector.js
+++ bin/docsector.js
@@ -23,7 +23,7 @@ const packageRoot = resolve(__dirname, '..')
 const args = process.argv.slice(2)
 const command = args[0]
 
-const VERSION = '0.8.0'
+const VERSION = '0.8.1'
 
 const HELP = `
   Docsector Reader v${VERSION}
